7ed7d3a278d0f8cf3a467352a846a49197c4b67f
Josh Nothum <joshnothum@gmail.com>
2017-10-09 13:54:05 -0500

    added appendDiv funtion

--- client.js
+++ client.js
@@ -2,5 +2,13 @@ $(document).ready(readyNow);
 
 function readyNow() {
   console.log('We are Sourced!');
+  clickHandler();
+
+}
+function clickHandler(){
+  $("#generate").on('click', appendDiv);
+}
+function appendDiv() {
+  $(".buttonHolder").append('<div>'+"Hello!"+'</div>');
 
 }
