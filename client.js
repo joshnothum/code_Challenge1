ec2948e3f9e56899470ee38329b95e761155f386
Josh Nothum <joshnothum@gmail.com>
2017-10-09 14:54:43 -0500

    finished css. need to change buttons

--- client.js
+++ client.js
@@ -7,10 +7,29 @@ function readyNow() {
 }
 function clickHandler(){
   $("#generate").on('click', appendDiv);
+  $("#generate").on('click', clickCounter);
+  $("#container").on('click',"#swapDiv" ,colorChanger);
+  $("#container").on('click',"#deleteDiv", deleteDiv);
 }
+var clicks = 0;
+
+function clickCounter(){
+  clicks = clicks+1;
+  return clicks;
+}
+
 function appendDiv() {
   //var $div= $(".buttonHolder").append('<div>'"hello"'</div>');
-  var $div = $('<div>"hello!"</div>');
-  $div.append('<p></p>');
+  var $div = $('<div class ="newDiv">"hello!"</div>');
+  $div.append('<p class="clicks">'+ "Clicks:" + clicks + '</p>');
+  console.log(clicks);
+  $div.append('<button id ="swapDiv">'+"Swap"+'</button>');
+  $div.append('<button id="deleteDiv">' +"Delete"+ '</button>');
 $(".buttonHolder").append($div);
+}
+function colorChanger(){
+  $('.newDiv').css("background-color","yellow");
+}
+function delteDiv(){
+  $(".newDiv").remove();
 }
